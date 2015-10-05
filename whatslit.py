1ebafd3b4d595e7bec971fdeaf2e4592b7b06988
Kunal Gosar <gosarkunal9@gmail.com>
2015-10-05 11:18:36 -0700

    configured website with google cloud platform to run online

---
+++ whatslit.py
@@ -0,0 +1,16 @@
+from google.appengine.api import users
+
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+            self.response.write('Hello, ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
