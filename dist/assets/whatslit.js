10248498a79a49817b2aa8391d853a09e30e3583
William Guss <carlostnb@gmail.com>
2015-10-15 18:16:14 -0700

    Cleaning up production.

--- dist/assets/whatslit.js
+++ dist/assets/whatslit.js
@@ -130,8 +130,8 @@ define('whatslit/templates/application', ['exports'], function (exports) {
             "column": 0
           },
           "end": {
-            "line": 4,
-            "column": 0
+            "line": 1,
+            "column": 36
           }
         },
         "moduleName": "whatslit/templates/application.hbs"
@@ -146,21 +146,11 @@ define('whatslit/templates/application', ['exports'], function (exports) {
         var el2 = dom.createTextNode("Welcome to Ember");
         dom.appendChild(el1, el2);
         dom.appendChild(el0, el1);
-        var el1 = dom.createTextNode("\n\n");
-        dom.appendChild(el0, el1);
-        var el1 = dom.createComment("");
-        dom.appendChild(el0, el1);
-        var el1 = dom.createTextNode("\n");
-        dom.appendChild(el0, el1);
         return el0;
       },
-      buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
-        var morphs = new Array(1);
-        morphs[0] = dom.createMorphAt(fragment,2,2,contextualElement);
-        return morphs;
-      },
+      buildRenderNodes: function buildRenderNodes() { return []; },
       statements: [
-        ["content","outlet",["loc",[null,[3,0],[3,10]]]]
+
       ],
       locals: [],
       templates: []
@@ -290,7 +280,7 @@ catch(err) {
 if (runningTests) {
   require("whatslit/tests/test-helper");
 } else {
-  require("whatslit/app")["default"].create({"name":"whatslit","version":"0.0.0+e411c2a9"});
+  require("whatslit/app")["default"].create({"name":"whatslit","version":"0.0.0+97d1e328"});
 }
 
 /* jshint ignore:end */
