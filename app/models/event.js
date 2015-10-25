b3b926d6e1e0dfc21fb8a59cd34d16fd177ddde1
William Guss <carlostnb@gmail.com>
2015-10-25 02:40:38 -0700

    Ammending error with event data model.

--- app/models/event.js
+++ app/models/event.js
@@ -5,7 +5,7 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   event_type: DS.attr('string'),
   was_remove: DS.attr('boolean', { defaultValue: false}),
-  time_posted: DS.attr('date');
+  time_posted: DS.attr('date'),
   description: DS.attr('description'),
   //comments: DS.
   latitude: DS.attr('number'),
